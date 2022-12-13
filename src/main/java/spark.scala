e2cab5a6e5d11ada08e640b1e7ed86f45259b146
omerfarukpolat <omerfarukpolat@etu.edu.tr>
2022-12-13 20:09:18 +0300

    motorlari maviliklere surecegiz

--- src/main/java/spark.scala
+++ src/main/java/spark.scala
@@ -1,6 +1,8 @@
-import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.functions._
+
+import Console._
 
 object spark {
   def main(args: Array[String]): Unit = {
@@ -12,24 +14,44 @@ object spark {
       .config("spark.master", "local")
       .getOrCreate()
 
-    val players = spark.read.format("csv")
-      .option("header", "true")
-      .load("/Users/kemaldemirel/Desktop/Lectures/BİL 401/Dataset/players_1920_fin.csv")
+    val players = spark.read.option("header", true).csv("players_1920_fin.csv")
+    val matches = spark.read.option("header", true).csv("epl2020.csv")
 
-    val epl = spark.read
-      .format("csv")
-      .option("header", "true")
-      .load("/Users/kemaldemirel/Desktop/Lectures/BİL 401/Dataset/epl2020.csv")
-
-    def MostAgressivePlayerToParticularTeam( team_name : String) : Dataset[Row] = {
-      return players.select("element", "full", "red_cards", "opponent_team")
-        .filter(col("opponent_team") === lit(team_name))
-        .groupBy("element","full")
-        .agg(sum("red_cards"))
-        .orderBy(desc("sum(red_cards)"))
+    def getMostAggresivePlayerAgainstX = (players: DataFrame, opponent: String) => {
+      println("Players with the most cards against ", opponent)
+      players.select("element", "full", "yellow_cards", "red_cards", "opponent_team")
+        .where(col("opponent_team") === opponent and col("yellow_cards") > 0)
+        .groupBy("element", "full").agg(sum("yellow_cards").name("totalYellowCards"), sum("red_cards").name("totalRedCards"))
+        .sort(desc("totalYellowCards"))
+        .show(false)
     }
 
-    val maxCard = epl
+    def getMostAggresivePlayer = (players: DataFrame) => {
+      println("Players with the most cards")
+      players.select("element", "full", "yellow_cards", "red_cards", "opponent_team")
+        .where(col("yellow_cards") > 0)
+        .groupBy("element", "full").agg(sum("yellow_cards").name("totalYellowCards"), sum("red_cards")
+        .name("totalRedCards"))
+        .sort(desc("totalYellowCards"))
+        .show(false)
+    }
+
+    def performanceBetweenTwoWeeks = (players: DataFrame, playerName: String, from: Integer, to: Integer) => {
+      val output = players.select("full", "round", "element", "goals_scored", "creativity")
+        .where(col("round") >= from
+          and col("round") <= to
+          and col("goals_scored") > 0)
+        .groupBy("full", "element")
+        .agg(sum("goals_scored").name("score"),
+          avg("creativity").name("creativity"))
+        .sort(desc("score"))
+        .drop("element")
+      output.show()
+      output.write.format("csv").save("/Users/omerfarukpolat/Spark-EnglishPremiereLeauge/src/main/output")
+
+    }
+
+    val maxCard = matches
       .select("`Referee.x`","`HR.x`","teamId", "`h_a`")
       .filter("h_a == 'h'")
       .groupBy("`Referee.x`")
@@ -38,7 +60,7 @@ object spark {
       .agg(max("totalCard"))
       .collect()(0)
 
-    val ev_sahibine_en_cok_kirmizi = epl
+    val ev_sahibine_en_cok_kirmizi = matches
       .select("`Referee.x`","`HR.x`","teamId","`h_a`")
       .filter("h_a == 'h'")
       .groupBy("`Referee.x`")
@@ -48,7 +70,7 @@ object spark {
 
 
 
-    val angry_referee = epl
+    val angry_referee = matches
       .select("`Referee.x`","`HR.x`","`HY.x`","`AY.x`","`AR.x`")
       .withColumn("totalCard", col("`HR.x`") + col("`HY.x`") + col("`AY.x`") + col("`AR.x`"))
       .groupBy("`Referee.x`")
@@ -56,9 +78,12 @@ object spark {
       .orderBy(desc("`numOfCards`"))
       .take(1)
 
-    println("The Angriest Referee" + angry_referee(0))
-    ev_sahibine_en_cok_kirmizi.show()
-    print(MostAgressivePlayerToParticularTeam("Crystal Palace").show()  )
+    // println("The Angriest Referee" + angry_referee(0))
+    //  ev_sahibine_en_cok_kirmizi.show()
+    // print(getMostAggresivePlayerAgainstX(players,"Crystal Palace"))
+    // print(getMostAggresivePlayer(players))
+
+    performanceBetweenTwoWeeks(players, "Raheem Sterling", 1, 17)
 
 
   }
