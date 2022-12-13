aaa48d6934795593b6c42ab4e6826c491757c327
Kemal Demirel <kmldemirel99@gmail.com>
2022-12-13 18:51:07 +0300

    First 3 functions Added

--- src/main/java/spark.scala
+++ src/main/java/spark.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions._
 
 object spark {
@@ -11,19 +12,22 @@ object spark {
       .config("spark.master", "local")
       .getOrCreate()
 
-    val players = spark.read.format("csv").option("header", "true").load("/Users/kemaldemirel/Desktop/Lectures/BİL 401/Dataset/players_1920_fin.csv")
-
-    //players.select("element", "full", "red_cards", "opponent_team")
-    // .filter("opponent_team == 'Crystal Palace'")
-    // .groupBy("element","full").count()
-    //.agg(sum("red_cards"))
-      //.orderBy(desc("sum(minutes)"))
-     // .show()
+    val players = spark.read.format("csv")
+      .option("header", "true")
+      .load("/Users/kemaldemirel/Desktop/Lectures/BİL 401/Dataset/players_1920_fin.csv")
 
     val epl = spark.read
       .format("csv")
       .option("header", "true")
       .load("/Users/kemaldemirel/Desktop/Lectures/BİL 401/Dataset/epl2020.csv")
+
+    def MostAgressivePlayerToParticularTeam( team_name : String) : Dataset[Row] = {
+      return players.select("element", "full", "red_cards", "opponent_team")
+        .filter(col("opponent_team") === lit(team_name))
+        .groupBy("element","full")
+        .agg(sum("red_cards"))
+        .orderBy(desc("sum(red_cards)"))
+    }
 
     val maxCard = epl
       .select("`Referee.x`","`HR.x`","teamId", "`h_a`")
@@ -34,14 +38,14 @@ object spark {
       .agg(max("totalCard"))
       .collect()(0)
 
-    epl
+    val ev_sahibine_en_cok_kirmizi = epl
       .select("`Referee.x`","`HR.x`","teamId","`h_a`")
       .filter("h_a == 'h'")
       .groupBy("`Referee.x`")
       .agg(sum("`HR.x`")
         .as("totalCard"))
       .filter(col("totalCard") === lit(maxCard(0)))
-      .show()
+
 
 
     val angry_referee = epl
@@ -53,8 +57,8 @@ object spark {
       .take(1)
 
     println("The Angriest Referee" + angry_referee(0))
-
-
+    ev_sahibine_en_cok_kirmizi.show()
+    print(MostAgressivePlayerToParticularTeam("Crystal Palace").show()  )
 
 
   }
